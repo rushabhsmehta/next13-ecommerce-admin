a59f86e78ed1393ca3a3949feef8f5b7e610e025
rushabhsmehta <rushabhsmehat@gmail.com>
2024-01-06 17:43:09 +0530

    .

--- app/(dashboard)/[storeId]/(routes)/tourPackageQueryDisplay/[tourPackageQueryId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/tourPackageQueryDisplay/[tourPackageQueryId]/page.tsx
@@ -55,7 +55,6 @@ const tourPackageQueryPage = async ({
 
   return (
     <>
-    <Navbar />
       <div className="flex-col">
       {/*  <div className="flex-1 space-y-4 p-8 pt-6">
         <TourPackageQueryForm

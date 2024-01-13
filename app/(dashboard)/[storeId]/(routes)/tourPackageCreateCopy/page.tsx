76ae8c63388214e2f16862bbee3c43b2d79ad56d
rushabhsmehta <rushabhsmehat@gmail.com>
2024-01-13 21:53:00 +0530

    .

---
+++ app/(dashboard)/[storeId]/(routes)/tourPackageCreateCopy/page.tsx
@@ -0,0 +1,64 @@
+import { format } from "date-fns";
+
+import prismadb from "@/lib/prismadb";
+import { formatter } from "@/lib/utils";
+import { TourPackageClient } from "./components/client";
+import { TourPackageColumn } from "./components/columns";
+import Navbar from "@/components/navbar";
+
+const tourPackagePage = async ({
+  params
+}: {
+  params: { storeId: string }
+}) => {
+  const tourPackage = await prismadb.tourPackage.findMany({
+    where: {
+      storeId: params.storeId
+    },
+    include: {
+      images: true,
+      location : true,
+      flightDetails: true,
+      itineraries: {
+        
+        include: {
+          itineraryImages: true,
+          activities:
+          {
+            include:
+            {
+              activityImages: true,
+            }
+          }
+        },
+        orderBy: {
+          days: 'asc' // or 'desc', depending on the desired order
+        }
+      }
+    }
+  });
+
+  const formattedtourPackage : TourPackageColumn[] = tourPackage.map((item) => ({
+    id: item.id,
+    tourPackageName : item.tourPackageName ?? '',
+    isFeatured: item.isFeatured,
+    isArchived: item.isArchived,
+    price: item.price ?? '',
+    location: item.location.label,
+    //hotel: item.hotel.name,
+    createdAt: format(item.createdAt, 'MMMM do, yyyy'),
+  }));
+
+  return (
+    <>
+    <Navbar />
+      <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <TourPackageClient data={formattedtourPackage} />
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default tourPackagePage;

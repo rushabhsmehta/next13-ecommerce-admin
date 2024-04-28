b47326f92e98705195d4fe6f789566dd813377a4
rushabhsmehta <rushabhsmehat@gmail.com>
2024-04-28 20:59:51 +0530

    .

---
+++ app/api/[storeId]/searchTermLocations/[searchTerm]/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs";
+
+import prismadb from "@/lib/prismadb";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { searchTerm : string } }
+) {
+  try {
+    if (!params.searchTerm) {
+      return new NextResponse("Search Term is required", { status: 400 });
+    }
+
+    const searchTerm = params.searchTerm.toLowerCase();
+
+    const locations = await prismadb.location.findMany({
+      where: {
+        tags : {
+          contains: searchTerm,
+        },
+      },
+    });
+  
+    return NextResponse.json(locations);
+  } catch (error) {
+    console.log('[LOCATION_GET]', error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+};

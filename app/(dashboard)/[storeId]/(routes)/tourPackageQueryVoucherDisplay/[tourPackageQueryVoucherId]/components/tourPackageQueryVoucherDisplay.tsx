f2cec862c0d0175989b560a7c22e30f4faaa4290
rushabhsmehta <rushabhsmehat@gmail.com>
2024-07-25 16:47:19 +0530

    ..

--- app/(dashboard)/[storeId]/(routes)/tourPackageQueryVoucherDisplay/[tourPackageQueryVoucherId]/components/tourPackageQueryVoucherDisplay.tsx
+++ app/(dashboard)/[storeId]/(routes)/tourPackageQueryVoucherDisplay/[tourPackageQueryVoucherId]/components/tourPackageQueryVoucherDisplay.tsx
@@ -237,7 +237,7 @@ export const TourPackageQueryVoucherDisplay: React.FC<TourPackageQueryVoucherDis
           <Card key={index} className="mb-4 break-inside-avoid">
             <CardHeader className="d-flex align-items-center">
               <span>Day: {itinerary.dayNumber} : {itinerary.days} - </span>
-              <div dangerouslySetInnerHTML={{ __html: itinerary.itineraryTitle || '' }} />
+              <div className="font-bold flex-grow-1" dangerouslySetInnerHTML={{ __html: itinerary.itineraryTitle || '' }} />
             </CardHeader>
           </Card>
         ))

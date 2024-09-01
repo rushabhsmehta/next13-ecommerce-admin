5584a7aaa77f0c1f9de4025695acfe0caefc7bf2
rushabhsmehta <rushabhsmehat@gmail.com>
2024-09-01 16:42:03 +0530

    .

--- components/ViewMyPDF.tsx
+++ components/ViewMyPDF.tsx
@@ -3,43 +3,48 @@
 import dynamic from "next/dynamic";
 import GenerateMyPDF from "./GenerateMyPDF";
 import { TourPackageQuery, Images, Itinerary, Activity, FlightDetails, Location, Hotel } from "@prisma/client"
+import { useSearchParams } from "next/navigation";
 
 
 interface ViewMyPDFProps {
-    data: TourPackageQuery & {
-        images: Images[];
-        itineraries: (Itinerary & {
-          itineraryImages: Images[];
-          activities: (Activity & {
-            activityImages: Images[];
-          })[] | null;
-        })[] | null;
-        flightDetails: FlightDetails[];
-      } | null;
-      locations: Location[] | null;
-      hotels: (Hotel & {
-        images: Images[];
+  data: TourPackageQuery & {
+    images: Images[];
+    itineraries: (Itinerary & {
+      itineraryImages: Images[];
+      activities: (Activity & {
+        activityImages: Images[];
       })[] | null;
-    
+    })[] | null;
+    flightDetails: FlightDetails[];
+  } | null;
+  locations: Location[] | null;
+  hotels: (Hotel & {
+    images: Images[];
+  })[] | null;
+
 }
 
 const PDFViewer = dynamic(
-    () => import("@react-pdf/renderer").then((mod) => mod.PDFViewer),
-    {
-        ssr: false,
-        loading: () => <p>Loading...</p>,
-    },
+  () => import("@react-pdf/renderer").then((mod) => mod.PDFViewer),
+  {
+    ssr: false,
+    loading: () => <p>Loading...</p>,
+  },
 );
 
+const searchParams = useSearchParams();
+const selectedOption = searchParams.get('search') || 'Empty'; // 'option' is the name of your query parameter
+
+
 const ViewMyPDF: React.FC<ViewMyPDFProps> = ({
-    data,
-    locations,
-    hotels,
+  data,
+  locations,
+  hotels,
 }) => (
 
-    <PDFViewer style={{ width: "100%", height: "100vh" }}>
-        <GenerateMyPDF data ={data} locations ={locations} hotels={hotels}/>
-    </PDFViewer>
+  <PDFViewer style={{ width: "100%", height: "100vh" }}>
+    <GenerateMyPDF data={data} locations={locations} hotels={hotels} selectedOption = { selectedOption} />
+  </PDFViewer>
 )
 
 export default ViewMyPDF;

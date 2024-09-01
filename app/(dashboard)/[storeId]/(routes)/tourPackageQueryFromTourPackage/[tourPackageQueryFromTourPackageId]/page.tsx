e355ba4a30c4d83d1689d093804aad13ca7e5a1d
rushabhsmehta <rushabhsmehat@gmail.com>
2024-09-01 17:18:45 +0530

    .

--- app/(dashboard)/[storeId]/(routes)/tourPackageQueryFromTourPackage/[tourPackageQueryFromTourPackageId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/tourPackageQueryFromTourPackage/[tourPackageQueryFromTourPackageId]/page.tsx
@@ -56,6 +56,20 @@ const tourPackageQueryFromTourPackagePage = async ({
   }
   );
 
+  const itinerariesMaster = await prismadb.itineraryMaster.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    include: {
+      itineraryMasterImages: true,
+      activities: {
+        include: {
+          activityImages: true,
+        }
+      },
+    }
+  });
+
   return (
     <><Navbar />
       <div className="flex-col">
@@ -64,7 +78,8 @@ const tourPackageQueryFromTourPackagePage = async ({
             initialData={tourPackage}
             locations={locations}
             hotels={hotels}
-            activitiesMaster={activitiesMaster} />
+            activitiesMaster={activitiesMaster}
+            itinerariesMaster={itinerariesMaster} />
         </div>
       </div></>
   );

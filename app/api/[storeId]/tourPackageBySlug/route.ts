b9e482ea9b437363b0676ea66a614eed12fad974
rushabhsmehta <rushabhsmehat@gmail.com>
2024-09-16 10:07:17 +0530

    .

---
+++ app/api/[storeId]/tourPackageBySlug/route.ts
@@ -0,0 +1,53 @@
+import { NextResponse } from 'next/server';
+import { auth } from '@clerk/nextjs';
+
+import prismadb from '@/lib/prismadb';
+export const dynamic = 'force-dynamic';
+
+export async function GET(
+    req: Request,
+    { params }: { params: { storeId: string } },
+) {
+    try {
+        const { searchParams } = new URL(req.url)
+        const slug = searchParams.get('slug') || undefined;
+        //  const hotelId = searchParams.get('hotelId') || undefined;
+        const isFeatured = searchParams.get('isFeatured');
+
+        if (!params.storeId) {
+            return new NextResponse("Store id is required", { status: 400 });
+        }
+
+        const tourPackage = await prismadb.tourPackage.findMany({
+            where: {
+                storeId: params.storeId,
+                slug,
+                //hotelId,
+                isFeatured: isFeatured ? true : undefined,
+                isArchived: false,
+            },
+            include: {
+                images: true,
+                location: true,
+                itineraries: {
+                    include: {
+                        itineraryImages: true,
+                        activities: {
+                            include: {
+                                activityImages: true,
+                            },
+                        },
+                    },
+                },
+            },
+            orderBy: {
+                createdAt: 'desc',
+            }
+        });
+
+        return NextResponse.json(tourPackage);
+    } catch (error) {
+        console.log('[TOUR_PACKAGES_GET]', error);
+        return new NextResponse("Internal error", { status: 500 });
+    }
+};

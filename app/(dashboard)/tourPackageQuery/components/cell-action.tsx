9b7510ba173270095b1f7b59cbd3dd97bfc1a076
rushabhsmehta <rushabhsmehat@gmail.com>
2024-11-10 21:16:50 +0530

    .

--- app/(dashboard)/tourPackageQuery/components/cell-action.tsx
+++ app/(dashboard)/tourPackageQuery/components/cell-action.tsx
@@ -120,7 +120,7 @@ export const CellAction: React.FC<CellActionProps> = ({
             </DropdownMenuSubTrigger>
             <DropdownMenuPortal>
               <DropdownMenuSubContent className="w-56">
-                <DropdownMenuItem onSelect={() => router.push(`/accounnts/${data.id}`)}>
+                <DropdownMenuItem onSelect={() => router.push(`/accounts/${data.id}`)}>
                   Make Entries
                 </DropdownMenuItem>
                 <DropdownMenuItem onSelect={() => router.push(`/fetchaccounts/${data.id}`)}>

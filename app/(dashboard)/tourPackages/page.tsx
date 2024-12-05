bc2bd97953c04879f1b6f5e820c836a65c866d24
rushabhsmehta <rushabhsmehat@gmail.com>
2024-12-05 16:10:44 +0530

    .

--- app/(dashboard)/tourPackages/page.tsx
+++ app/(dashboard)/tourPackages/page.tsx
@@ -10,37 +10,38 @@ const tourPackagesPage = async ({
 
 }) => {
   const tourPackages = await prismadb.tourPackage.findMany({
-    
+
     include: {
       location: true,
-    //  hotel : true,
-   
+      //  hotel : true,
+
     },
     orderBy: {
-      updatedAt : 'desc'
+      updatedAt: 'desc'
     }
   });
 
   const formattedtourPackages: TourPackageColumn[] = tourPackages.map((item) => ({
     id: item.id,
     tourPackageName: item.tourPackageName ?? '',
-    tourPackageType : item.tourPackageType ?? '',
+    tourPackageType: item.tourPackageType ?? '',
     isFeatured: item.isFeatured,
     isArchived: item.isArchived,
     price: item.price ?? '',
     location: item.location.label,
-  //  hotel: item.hotel.name,
+    //  hotel: item.hotel.name,
     createdAt: format(item.createdAt, 'MMMM do, yyyy'),
+    updatedAt: format(item.updatedAt, 'MMMM do, yyyy'),
   }));
 
   return (
     <>
-    <Navbar />
+      <Navbar />
       <div className="flex-col">
-      <div className="flex-1 space-y-4 p-8 pt-6">
-        <TourPackagesClient data={formattedtourPackages} />
+        <div className="flex-1 space-y-4 p-8 pt-6">
+          <TourPackagesClient data={formattedtourPackages} />
+        </div>
       </div>
-    </div>
     </>
   );
 };

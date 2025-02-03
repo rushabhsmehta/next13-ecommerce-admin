d15ed54eb15b760e5e870e6fb449ca78ca9de968
rushabhsmehta <rushabhsmehat@gmail.com>
2025-02-03 23:09:41 +0530

    .

--- app/(dashboard)/tourPackageCreateCopy/page.tsx
+++ app/(dashboard)/tourPackageCreateCopy/page.tsx
@@ -1,7 +1,6 @@
 import { format } from "date-fns";
 
 import prismadb from "@/lib/prismadb";
-import { formatter } from "@/lib/utils";
 import { TourPackageClient } from "./components/client";
 import { TourPackageColumn } from "./components/columns";
 import Navbar from "@/components/navbar";

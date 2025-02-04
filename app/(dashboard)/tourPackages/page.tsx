0823b97eaaae2892850758113008483eb737aaf4
rushabhsmehta <rushabhsmehat@gmail.com>
2025-02-04 21:58:59 +0530

    .

--- app/(dashboard)/tourPackages/page.tsx
+++ app/(dashboard)/tourPackages/page.tsx
@@ -39,7 +39,6 @@ const tourPackagesPage = async ({
     <>
       {/*       <Navbar /> */}
       
-        <AppSidebar/>
       <div className="flex-col">
         <div className="flex-1 space-y-4 p-8 pt-6">
           <TourPackagesClient data={formattedtourPackages} />

7a8a1bc2f7dc87a83a62a8822d514c76c9f83720
rushabhsmehta <rushabhsmehat@gmail.com>
2025-02-08 16:30:34 +0530

    .

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,22 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 export default authMiddleware({
-  publicRoutes: ["/api/:path*","/tourPackageQueryDisplay/:path*", "/generatePDFfromURL/:path*","/tourPackageQueryPDFGenerator/:path*"],
+  publicRoutes: [
+    "/api/:path*",
+    "/tourPackageQueryDisplay/:path*",
+    "/generatePDFfromURL/:path*",
+    "/tourPackageQueryPDFGenerator/:path*",
+  ],
+
+  async beforeAuth(req) {
+    const userAgent = req.headers.get("user-agent") || "";
+
+    // ✅ Allow Puppeteer (Headless Chrome) to bypass authentication
+    if (userAgent.includes("HeadlessChrome") || userAgent.includes("Puppeteer")) {
+      return NextResponse.next();
+    }
+  },
 });
 
 export const config = {

6ca81a545e1285dba44216af455eaea9f6b107cd
rushabhsmehta <rushabhsmehat@gmail.com>
2025-02-08 15:04:17 +0530

    .

--- app/(dashboard)/tourPackageQuery/components/cell-action.tsx
+++ app/(dashboard)/tourPackageQuery/components/cell-action.tsx
@@ -69,7 +69,7 @@ export const CellAction: React.FC<CellActionProps> = ({
     }
   
     // Construct the URL dynamically with `encodeURIComponent` for safety
-    const url = `/tourPackageQueryDisplay/${data.id}?search=${selectedOption}`;
+    const url = `https://next13-ecommerce-admin-zeta.vercel.app/tourPackageQueryDisplay/${data.id}?search=${selectedOption}`;
     
     // Open `generatePDFfromURL` with the provided URL as a query parameter
     router.push(`/generatePDFfromURL?url=${encodeURIComponent(url)}`);

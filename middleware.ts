a708b3fae17498584341247e9f453638a528bf0a
rushabhsmehta <rushabhsmehat@gmail.com>
2025-02-08 14:24:04 +0530

    .

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
-  publicRoutes: ["/api/:path*","/tourPackageQueryDisplay/:path*", "/tourPackageQueryPDFGenerator/:path*"],
+  publicRoutes: ["/api/:path*","/tourPackageQueryDisplay/:path*", "/generatePDFfromURL/:path*","/tourPackageQueryPDFGenerator/:path*"],
 });
 
 export const config = {

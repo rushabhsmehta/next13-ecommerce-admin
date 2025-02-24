69ca2de19c9dc7882f339cdd45b86c5cf434f0fb
rushabhsmehta <rushabhsmehat@gmail.com>
2025-02-24 18:41:45 +0530

    .

--- app/(dashboard)/customers/[customerId]/page.tsx
+++ app/(dashboard)/customers/[customerId]/page.tsx
@@ -13,11 +13,20 @@ const CustomerPage = async ({
     }
   });
 
+  // fetch associate partners
+  const associatePartners = await prismadb.associatePartner
+    .findMany({
+      orderBy: {
+        name: 'asc'
+      }
+    });
+
+
   return (
     <>
       <div className="flex flex-col">
         <div className="flex-1 space-y-4 p-8 pt-6">
-          <CustomerForm initialData={customer} />
+          <CustomerForm initialData={customer} associatePartners={associatePartners}  />
         </div>
       </div>
 

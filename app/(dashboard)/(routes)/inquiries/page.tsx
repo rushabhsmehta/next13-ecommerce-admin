1804d80d2e9f3204b38d157c7fcf53758566b3f5
rushabhsmehta <rushabhsmehat@gmail.com>
2025-02-24 22:33:55 +0530

    .

--- app/(dashboard)/(routes)/inquiries/page.tsx
+++ app/(dashboard)/(routes)/inquiries/page.tsx
@@ -56,9 +56,9 @@ const InquiriesPage = async ({ searchParams }: InquiriesPageProps) => {
         <div className="flex items-center justify-between">
           <h2 className="text-3xl font-bold tracking-tight">Inquiries</h2>
         </div>
-        <div className="flex items-center gap-x-4">
+      {/*   <div className="flex items-center gap-x-4">
           <StatusFilter />
-        </div>
+        </div> */}
         <InquiriesClient 
           data={formattedInquiries} 
           associates={associates}

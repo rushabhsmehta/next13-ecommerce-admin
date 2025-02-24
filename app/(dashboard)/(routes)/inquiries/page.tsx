b5735cc7b460dfee8b252a80fe88f3d9da8f32ee
rushabhsmehta <rushabhsmehat@gmail.com>
2025-02-24 22:26:33 +0530

    .

--- app/(dashboard)/(routes)/inquiries/page.tsx
+++ app/(dashboard)/(routes)/inquiries/page.tsx
@@ -1,10 +1,11 @@
-import { format } from "date-fns";
-import prismadb from "@/lib/prismadb";
 import { InquiriesClient } from "./components/client";
-import { InquiryColumn } from "./components/columns";
+import { StatusFilter } from "./components/status-filter";
+import prismadb from "@/lib/prismadb";
+import { format } from "date-fns";
 
 interface InquiriesPageProps {
   searchParams: {
+    status?: string;
     associateId?: string;
   }
 }
@@ -20,6 +21,9 @@ const InquiriesPage = async ({ searchParams }: InquiriesPageProps) => {
   const where = {
     ...(searchParams.associateId && {
       associatePartnerId: searchParams.associateId
+    }),
+    ...(searchParams.status && {
+      status: searchParams.status
     })
   };
 
@@ -34,7 +38,7 @@ const InquiriesPage = async ({ searchParams }: InquiriesPageProps) => {
     }
   });
 
-  const formattedInquiries: InquiryColumn[] = inquiries.map((item) => ({
+  const formattedInquiries = inquiries.map((item) => ({
     id: item.id,
     customerName: item.customerName,
     customerMobileNumber: item.customerMobileNumber,
@@ -49,6 +53,12 @@ const InquiriesPage = async ({ searchParams }: InquiriesPageProps) => {
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
+        <div className="flex items-center justify-between">
+          <h2 className="text-3xl font-bold tracking-tight">Inquiries</h2>
+        </div>
+        <div className="flex items-center gap-x-4">
+          <StatusFilter />
+        </div>
         <InquiriesClient 
           data={formattedInquiries} 
           associates={associates}

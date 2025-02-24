21adba02295ebf164d78c1b434e61f4284b881c8
rushabhsmehta <rushabhsmehat@gmail.com>
2025-02-24 19:45:09 +0530

    .

--- app/(dashboard)/(routes)/inquiries/components/client.tsx
+++ app/(dashboard)/(routes)/inquiries/components/client.tsx
@@ -2,22 +2,34 @@ import { DataTable } from "@/components/ui/data-table";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
 import { InquiryColumn, columns } from "./columns";
+import { useParams } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { Plus } from "lucide-react";
 
 interface InquiriesClientProps {
   data: InquiryColumn[];
 }
 
-export const InquiriesClient: React.FC<InquiriesClientProps> = ({ data }) => {
+export const InquiriesClient: React.FC<InquiriesClientProps> = ({
+  data
+}) => {
+  const params = useParams();
+
+  const handleAddNewClick = () => {
+    // Open the link in a new tab
+    window.open(`/inquiries/new`, '_blank');
+  };
+
   return (
     <>
       <div className="flex items-center justify-between">
-        <Heading
-          title={`Inquiries (${data.length})`}
-          description="Manage inquiries for your business"
-        />
+        <Heading title={`Inquiries (${data.length})`} description="Manage inquiries from your website" />
+        <Button onClick={handleAddNewClick}>
+          <Plus className="mr-2 h-4 w-4" /> Add New
+        </Button>
       </div>
       <Separator />
-      <DataTable searchKey="customerName" columns={columns} data={data} />
+      <DataTable searchKey="email" columns={columns} data={data} />
     </>
   );
 };

e6e1f7beff22c73367a7da3553e6030c20d03808
rushabhsmehta <rushabhsmehat@gmail.com>
2025-02-28 21:48:36 +0530

    .

--- app/(dashboard)/accounts/[tourPackageQueryId]/page.tsx
+++ app/(dashboard)/accounts/[tourPackageQueryId]/page.tsx
@@ -54,6 +54,7 @@ const tourPackageQueryPage = async ({
           />
         </div>
 
+
         {/*  <div className="flex-1 space-y-4 p-8 pt-6">
       <TourPackageQueryDisplay
         data={tourPackageQuery}

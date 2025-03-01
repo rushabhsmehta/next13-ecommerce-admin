a80972ab8f4fbcca27d8d8e0139bd795bb95abb2
rushabhsmehta <rushabhsmehat@gmail.com>
2025-03-01 17:59:44 +0530

    .

--- app/(dashboard)/tourPackageQuery/[tourPackageQueryId]/components/defaultValues.ts
+++ app/(dashboard)/tourPackageQuery/[tourPackageQueryId]/components/defaultValues.ts
@@ -72,9 +72,9 @@ export const USEFUL_TIPS_DEFAULT =
   "∎ We reserve the right to issue a full refund in case we believe that we are unable to fulfil the services for any technical reasons.";
 
 export const DISCLAIMER_DEFAULT =
-  "! The Prices mentioned are subject to availability at the time of confirmation.\n" +
-  "! Flight rates will be provided separately, as they are dynamic and may impact the total package cost\n" +
-  "! The rates quoted are valid for 7 days only";
+  "-> The Prices mentioned are subject to availability at the time of confirmation.\n" +
+  "-> Flight rates will be provided separately, as they are dynamic and may impact the total package cost\n" +
+  "-> The rates quoted are valid for 7 days only";
 
 
 export const TOUR_HIGHLIGHTS_DEFAULT = `

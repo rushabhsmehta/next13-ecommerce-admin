f1b4a905003090c35fd9941cc68e1f533cde08ed
rushabhsmehta <rushabhsmehat@gmail.com>
2025-03-08 18:46:04 +0530

    .

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+
 import { cn } from "@/lib/utils";
 import { ChevronRight, LayoutGrid, type LucideIcon } from "lucide-react";
 import {

aacc3d7eee8ce9e6d0c1b9658f5075c777c9ccd6
rushabhsmehta <rushabhsmehat@gmail.com>
2025-04-05 17:17:01 +0530

    .

--- src/app/(dashboard)/expense-categories/page.tsx
+++ src/app/(dashboard)/expense-categories/page.tsx
@@ -23,8 +23,6 @@ const ExpenseCategoriesPage = async () => {
   return (
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <Heading title="Expense Categories" description="Manage expense categories" />
-        <Separator />
         <ExpenseCategoryClient data={formattedExpenseCategories} />
       </div>
     </div>

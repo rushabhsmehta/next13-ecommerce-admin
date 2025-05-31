898cc88a6c6c525899408e3d2aa7b5b1a305d6e1
rushabhsmehta <rushabhsmehat@gmail.com>
2025-05-31 17:42:54 +0530

    feat: Add dynamic export to multiple API routes for improved performance

--- src/app/api/financial-records/route.ts
+++ src/app/api/financial-records/route.ts
@@ -2,6 +2,8 @@ import { NextResponse } from "next/server";
 import { auth } from "@clerk/nextjs";
 import prismadb from "@/lib/prismadb";
 
+export const dynamic = 'force-dynamic';
+
 export async function GET(req: Request) {
   try {
     const { userId } = auth();
